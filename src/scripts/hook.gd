8019952ef39f293a66300de78a608b0bedad92d1
Philippe Vo <philippe.vo.nam@gmail.com>
2023-11-24 14:16:34 -0500

    added max range

--- src/scripts/hook.gd
+++ src/scripts/hook.gd
@@ -3,14 +3,19 @@ extends Area2D
 var speed = 5000
 var hooked = false
 
+var launchedPosition = Vector2(0,0)
 var direction = 1
+var maxRange = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	launchedPosition = self.position
 
 func _physics_process(delta):
 	if not hooked:
+		if (position - launchedPosition).length() > maxRange:
+			self.queue_free()
+			
 		position += ((direction * transform.x) + -transform.y) * speed * delta
 
 func _on_body_entered(body):
