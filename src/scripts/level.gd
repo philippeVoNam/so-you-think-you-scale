5666402f473704c071e3bd291fdcae073b8b9ed1
Philippe Vo <philippe.vo.nam@gmail.com>
2023-11-26 12:46:22 -0500

    added start and end with time run

--- src/scripts/level.gd
+++ src/scripts/level.gd
@@ -4,15 +4,25 @@ var snowScene = preload("res://src/scenes/snow.tscn")
 var snow = null
 var _timer = null
 
+var timeReachGoal = 0
+var timeStart = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	timeStart = Time.get_unix_time_from_system()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 #	await get_tree().create_timer(3.0).timeout # waits for 1 second
 #	generate_snow()
 	pass
+	
+func restart():
+	pass
+	
+func finished():
+	timeReachGoal = Time.get_unix_time_from_system() - timeStart
+	print("finished run in : " + str(timeReachGoal) + " seconds")
 	
 func generate_snow():
 	self.snow = snowScene.instantiate()
